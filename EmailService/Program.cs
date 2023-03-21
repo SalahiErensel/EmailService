d5fcd9d696ab0b561616e0bc9873d12a743b1df1
Salahi Erensel <salahi_erensel@hotmail.com>
2023-03-21 10:39:42 +0200

    Add project files.

---
+++ EmailService/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Mail;
+
+namespace EmailService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+            //From email will be entered here
+            mail.From = new MailAddress("example@gmail.com");
+            //To email will be entered here
+            mail.To.Add("example@gmail.com");
+            //Email subject
+            mail.Subject = "Test";
+            //Email body
+            mail.Body = "Test";
+            smtp.Port = 587;
+            //Email from and password next to it will be entered here
+            smtp.Credentials = new System.Net.NetworkCredential("example@gmail.com", "password");
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
+        }
+    }
+}
